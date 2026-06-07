b910db0db33fa1ec05a87d5e5b127e484811fc09
MalhonDev <dev.malhon@gmail.com>
2026-06-07 15:17:24 -0300

    catalogo-full

--- src/data/catalogo.ts
+++ src/data/catalogo.ts
@@ -4,7 +4,7 @@ export type Marca = {
   id: string;
   name: string;
   logo: string;
-  size?: "md" | "lg" | "xl";
+  size?: "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl" | "6xl";
 };
 
 export type UnidadeCatalogo = {
